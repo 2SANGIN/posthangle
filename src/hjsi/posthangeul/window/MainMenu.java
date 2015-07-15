e3a16c81faee30a2e7d85a1f4084a8143d500435
Sangin Lee <silee9019@gmail.com>
2015-07-15 22:02:48 +0900

    메뉴 아이템 테스트 중

--- src/hjsi/posthangeul/window/MainMenu.java
+++ src/hjsi/posthangeul/window/MainMenu.java
@@ -1,9 +1,11 @@
 package hjsi.posthangeul.window;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MainMenu extends JMenuBar {
   String[] menuNames = {"File", "Help"};
@@ -13,8 +15,17 @@ public class MainMenu extends JMenuBar {
     super();
 
     menus = new ArrayList<JMenu>();
+
+
     JMenu temp = new JMenu("File");
     temp.setMnemonic('F');
+    temp.setBackground(Color.WHITE);
+    temp.add(new JMenuItem("New"));
+    temp.add(new JMenuItem("Open"));
+    temp.addSeparator();
+    temp.add(new JMenuItem("Save"));
+    temp.add(new JMenuItem("Save as..."));
+
     menus.add(temp);
 
     for (JMenu menu : menus) {
