034a24ddf9bf89256b09ddd075f667bc0f8bbd2f
Sangin Lee <silee9019@gmail.com>
2015-07-16 23:49:24 +0900

    menubar modified a little.

--- src/hjsi/posthangeul/window/MainMenu.java
+++ src/hjsi/posthangeul/window/MainMenu.java
@@ -1,6 +1,5 @@
 package hjsi.posthangeul.window;
 
-import java.awt.Color;
 import java.util.ArrayList;
 
 import javax.swing.JMenu;
@@ -8,25 +7,50 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 public class MainMenu extends JMenuBar {
-  String[] menuNames = {"File", "Help"};
   ArrayList<JMenu> menus;
+  JMenu menuFile;
+  JMenu menuEdit;
+  JMenu menuWindow;
+  JMenu menuHelp;
 
   public MainMenu() {
     super();
 
     menus = new ArrayList<JMenu>();
 
+    menuFile = new JMenu("File");
+    menuFile.setMnemonic('F');
+    menuEdit = new JMenu("Edit");
+    menuEdit.setMnemonic('E');
+    menuWindow = new JMenu("Window");
+    menuWindow.setMnemonic('W');
+    menuHelp = new JMenu("Help");
+    menuHelp.setMnemonic('H');
 
-    JMenu temp = new JMenu("File");
-    temp.setMnemonic('F');
-    temp.setBackground(Color.WHITE);
-    temp.add(new JMenuItem("New"));
-    temp.add(new JMenuItem("Open"));
-    temp.addSeparator();
-    temp.add(new JMenuItem("Save"));
-    temp.add(new JMenuItem("Save as..."));
+    menuFile.add(new JMenuItem("New"));
+    menuFile.add(new JMenuItem("Open"));
+    menuFile.addSeparator();
+    menuFile.add(new JMenuItem("Save"));
+    menuFile.add(new JMenuItem("Save as..."));
 
-    menus.add(temp);
+    menuEdit.add(new JMenuItem("Undo"));
+    menuEdit.add(new JMenuItem("Redo"));
+    menuEdit.addSeparator();
+    menuEdit.add(new JMenuItem("Cut"));
+    menuEdit.add(new JMenuItem("Copy"));
+    menuEdit.add(new JMenuItem("Paste"));
+    menuEdit.add(new JMenuItem("Delete"));
+
+    menuWindow.add(new JMenuItem("KeyCode Viewer"));
+    menuWindow.add(new JMenuItem("Preference"));
+
+    menuHelp.add(new JMenuItem("Help"));
+    menuHelp.add(new JMenuItem("About"));
+
+    menus.add(menuFile);
+    menus.add(menuEdit);
+    menus.add(menuWindow);
+    menus.add(menuHelp);
 
     for (JMenu menu : menus) {
       add(menu);
