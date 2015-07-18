19d013cc2e03039ff159a1db681c26d4c730f7bb
Sangin <silee9019@gmail.com>
2015-07-18 23:40:06 +0900

    editor merged

--- src/hjsi/posthangeul/window/PostHangeulApp.java
+++ src/hjsi/posthangeul/window/PostHangeulApp.java
@@ -1,22 +1,40 @@
 package hjsi.posthangeul.window;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
 
 public class PostHangeulApp implements KeyListener {
   JFrame mainWindow;
+  JTextPane textPane = new JTextPane();
+  JScrollPane scrollPane = new JScrollPane(textPane);
   MainMenu menuBar;
   JDialog keyCodeViewer;
   JPanel panel;
   char keyChar = 0;
+
+  JPanel north = new JPanel();
+
+  JButton bold = new JButton();
+  JButton italic = new JButton();
+  JButton color = new JButton();
+  JComboBox font = new JComboBox();
+  JButton size = new JButton();
 
   public PostHangeulApp() {
     mainWindow = new JFrame("Post Hangeul");
@@ -55,8 +73,49 @@ public class PostHangeulApp implements KeyListener {
     mainWindow.setJMenuBar(menuBar);
     /* 메뉴바 생성 끝 */
 
-    // TODO 고칠 곳
-    // mainWindow.getContentPane().add(here);
+    bold.setText("B");
+    bold.setOpaque(false);
+    bold.setContentAreaFilled(false);
+
+    italic.setText("I");
+    italic.setOpaque(false);
+    italic.setContentAreaFilled(false);
+
+    color.setText("C");
+    color.setOpaque(false);
+    color.setContentAreaFilled(false);
+
+    Action boldAction = new BoldAction();
+    boldAction.putValue(Action.NAME, "Bold");
+    bold.addActionListener(boldAction);
+
+    Action italicAction = new ItalicAction();
+    italicAction.putValue(Action.NAME, "Italic");
+    italic.addActionListener(italicAction);
+
+    Action foregroundAction = new ForegroundAction();
+    foregroundAction.putValue(Action.NAME, "Color");
+    color.addActionListener(foregroundAction);
+
+    Action formatTextAction = new FontAction();
+    formatTextAction.putValue(Action.NAME, "Font");
+
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    String[] fontNames = ge.getAvailableFontFamilyNames();
+    for (int i = 0; i < fontNames.length; i++) {
+      font.addItem(fontNames[i]);
+    }
+    font.getSelectedItem();
+    font.addActionListener(formatTextAction);
+
+    north.add(bold);
+    north.add(italic);
+    north.add(color);
+    north.add(font);
+    north.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+    mainWindow.getContentPane().add(north, BorderLayout.NORTH);
+    mainWindow.getContentPane().add(scrollPane, BorderLayout.CENTER);
 
     /* 메인프레임 생성 */
     mainWindow.setBounds(0, 0, 600, 480);
@@ -64,6 +123,7 @@ public class PostHangeulApp implements KeyListener {
     mainWindow.setLocationRelativeTo(null);
     mainWindow.setVisible(true);
     /* 메인프레임 생성 끝 */
+    textPane.requestFocus();
   }
 
   public static void main(String[] args) {
