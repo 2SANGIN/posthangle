98dff426ca9b5edd496bc3a44eca21f925e2f123
Sangin <silee9019@gmail.com>
2015-08-11 22:40:24 +0900

    no message

--- src/hjsi/posthangeul/editor/AutoComplete.java
+++ src/hjsi/posthangeul/editor/AutoComplete.java
@@ -1,8 +1,9 @@
 package hjsi.posthangeul.editor;
 
 import java.awt.Rectangle;
-import java.awt.event.KeyAdapter;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
@@ -11,30 +12,52 @@ import javax.swing.text.BadLocationException;
 
 
 
-public class AutoComplete extends JPopupMenu{
-	private static final long serialVersionUID = 4592225249925286812L;
-	
-	public AutoComplete(JTextPane textPane) {
-		setOpaque(true);
-		
-		textPane.addKeyListener(new KeyAdapter() {
-	    	public void keyPressed(KeyEvent e) {
-	    	if(e.getKeyCode() == KeyEvent.VK_CONTROL) {
-	    		add(new JMenuItem("asd"));
-	    		add(new JMenuItem("asd2"));
-	    		try {
-					Rectangle caret = textPane.modelToView(textPane.getCaretPosition());
-					show(textPane, caret.x, caret.y + (int)caret.getHeight());
-				} catch (BadLocationException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-	    	}
-			else if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
-				removeAll();
-	    	}
-	    		
-		});
-	}
+public class AutoComplete extends JPopupMenu implements KeyListener {
+  private static final long serialVersionUID = 4592225249925286812L;
 
+  JTextPane textPane;
+  Rectangle anchor;
+
+  public AutoComplete(JTextPane textPane) {
+    setOpaque(true);
+    add(new JMenuItem("asd"));
+    add(new JMenuItem("asd2"));
+    this.textPane = textPane;
+    this.textPane.addKeyListener(this);
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    int modMask = e.getModifiers();
+    int keyCode = e.getKeyCode();
+
+    switch (e.getKeyCode()) {
+      case KeyEvent.VK_SPACE:
+        if ((modMask & InputEvent.CTRL_MASK) >= 1) {
+          setVisible(true);
+        }
+        break;
+    }
+
+    if (isShowing()) {
+      try {
+        anchor = textPane.modelToView(textPane.getCaretPosition());
+        show(textPane, anchor.x, anchor.y + (int) anchor.getHeight());
+      } catch (BadLocationException e1) {
+        e1.printStackTrace();
+      }
+    }
+    textPane.requestFocus();
+    textPane.setText(e.getKeyChar() + "");
+  }
 }
