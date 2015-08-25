f58a96318f4e5847dcd862fa1a960c7d32658f9a
HJ <hyunjin7857@gmail.com>
2015-08-25 13:41:37 +0900

    Redo Undo ctrl Z ctrl Y

--- src/hjsi/posthangeul/editor/SwiftEditor.java
+++ src/hjsi/posthangeul/editor/SwiftEditor.java
@@ -3,12 +3,19 @@ package hjsi.posthangeul.editor;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.KeyStroke;
 import javax.swing.event.CaretListener;
-
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 
 public class SwiftEditor extends JPanel {
    private static final long serialVersionUID = 8037738001573590413L;
@@ -19,6 +26,7 @@ public class SwiftEditor extends JPanel {
    JScrollPane scrollPane;
    SwitchLine switchLine;
    JTextPane textPane;
+   UndoManager undo = new UndoManager();
 
    {
       textPane = new JTextPane();
@@ -31,10 +39,56 @@ public class SwiftEditor extends JPanel {
       linenum.setBackground(Color.WHITE);
       scrollPane.setRowHeaderView(linenum);
 
+      /* redo undo */
+      textPane.getDocument().addUndoableEditListener(new UndoableEditListener() {
+
+         @Override
+         public void undoableEditHappened(UndoableEditEvent e) {
+            // TODO Auto-generated method stub
+            undo.addEdit(e.getEdit());
+         }
+      });
+
+      // undo action
+      textPane.getActionMap().put("Undo", new AbstractAction("Undo") {
+         private static final long serialVersionUID = -6318592051036637485L;
+
+         @Override
+         public void actionPerformed(ActionEvent evt) {
+            // TODO Auto-generated method stub
+            try {
+               if (undo.canUndo())
+                  undo.undo();
+            } catch (CannotUndoException e) {
+
+            }
+         }
+      });
+
+      // ctrl-z to undo action
+      textPane.getInputMap().put(KeyStroke.getKeyStroke("control Z"), "Undo");
+
+      // redo action
+      textPane.getActionMap().put("Redo", new AbstractAction("Redo") {
+
+         @Override
+         public void actionPerformed(ActionEvent evt) {
+            // TODO Auto-generated method stub
+            try {
+               if (undo.canRedo()) {
+                  undo.redo();
+               }
+            } catch (CannotRedoException e) {
+            }
+         }
+      });
+
+      // ctrl-y to redo action
+      textPane.getInputMap().put(KeyStroke.getKeyStroke("control Y"), "Redo");
+
       setLayout(new BorderLayout());
       add(scrollPane);
    }
-
 
    public void addCaretListener(CaretListener listener) {
       textPane.addCaretListener(listener);
