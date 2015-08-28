793762c02caae3323adef78f6ea88cee878f7370
HJ <hyunjin7857@gmail.com>
2015-08-28 20:27:52 +0900

    녹음기 데탑에서 안됨

--- src/hjsi/posthangeul/window/Recorder.java
+++ src/hjsi/posthangeul/window/Recorder.java
@@ -1,6 +1,10 @@
 package hjsi.posthangeul.window;
 
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -12,7 +16,9 @@ import java.util.Date;
 import javax.sound.sampled.LineUnavailableException;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
+import javax.swing.border.Border;
 
 import recorder.SoundRecordingUtil;
 
@@ -21,6 +27,11 @@ public class Recorder extends JPanel {
    JToolBar toolbar;
    JButton start;
    JButton stop;
+   JTextField msg;
+   
+   private int sec = 0;
+   private int min = 0;
+   private int mil = 0;
 
    public Recorder(PostHangeulApp app, int btnSize) {
 
@@ -29,12 +40,19 @@ public class Recorder extends JPanel {
       toolbar = new JToolBar();
       toolbar.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 0));
       start = new JButton("start");
-      start.setSize(btnSize, btnSize);
       stop = new JButton("stop");
+      msg = new JTextField("Recording is ready");
+      
+      start.setSize(btnSize, btnSize);
       stop.setSize(btnSize, btnSize);
+      msg.setPreferredSize(new Dimension(200, btnSize));
+      msg.setEditable(false);
+      msg.setBackground(getBackground());
+      msg.setBorder(null);
 
       toolbar.add(start);
       toolbar.add(stop);
+      toolbar.add(msg);
       toolbar.setFloatable(false);
 
       add(toolbar);
@@ -88,4 +106,35 @@ public class Recorder extends JPanel {
       Date date = new Date();
       return dateFormat.format(date);
    }
+   
+   private void count(){
+          long now = System.currentTimeMillis();
+          while(true){
+              if(System.currentTimeMillis()-now>=100){
+                  now=System.currentTimeMillis();
+                  String strSec = Integer.toString(sec);
+                  String strMin = Integer.toString(min);
+                  String strMil = Integer.toString(mil);
+                  update(strSec,sec,strMin,strMil,mil);
+                  mil++;
+                  if(mil>9){
+                      mil=0;
+                      sec++;
+                      if(sec>=60){
+                          sec=1;
+                          min++;
+                      }
+                  }
+          }
+      }
+  }
+   
+   private void update(String sec,int s, String min,String mil,int m){
+      if (s<=10){
+          sec="0"+sec;
+      }
+      System.out.println(min+":"+sec+","+mil);
+      msg.setText(min+":"+sec+","+mil);
+
+  }
 }
--- src/recorder/SoundRecordingUtil.java
+++ src/recorder/SoundRecordingUtil.java
@@ -102,4 +102,6 @@ public class SoundRecordingUtil {
       audioInputStream.close();
       recordBytes.close();
    }
+   
+   
 }
