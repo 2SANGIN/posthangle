38b61b57a0fa9ada5434fe6db1d20a28956f4e05
Lee Hyunjin <leehyunjin@Leeui-MacBook-Air.local>
2015-08-28 16:03:03 +0900

    고투라인 끝줄로 가게 수정 / 도움말 추가 / 녹음기 추가

--- src/hjsi/posthangeul/window/Helper.java
+++ src/hjsi/posthangeul/window/Helper.java
@@ -1,6 +1,7 @@
 package hjsi.posthangeul.window;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Point;
@@ -19,6 +20,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
+import javax.swing.text.Element;
 
 
 public class Helper extends JPanel implements KeyListener {
@@ -28,13 +30,22 @@ public class Helper extends JPanel implements KeyListener {
    private JTextPane editor;
    private JLabel text;
 
+   private int BackSpaceCount;
+   private int lineCount;
+   private boolean isStart;
+
    public Helper(JTextPane textPane) {
       close = new JButton();
-      text = new JLabel("Ctrl BackSpace로 전부 지우소!!");
+      text = new JLabel();
       editor = textPane;
+      BackSpaceCount = 0;
+      lineCount = 0;
+      isStart = true;
+
       try {
-         bi = ImageIO.read(new File("resources", "close-icon.png")).getScaledInstance(20, 20,
-               Image.SCALE_AREA_AVERAGING);
+         bi =
+               ImageIO.read(new File("resources", "close-icon.png")).getScaledInstance(20, 20,
+                     Image.SCALE_AREA_AVERAGING);
       } catch (IOException e1) {
          // TODO Auto-generated catch block
          e1.printStackTrace();
@@ -45,6 +56,7 @@ public class Helper extends JPanel implements KeyListener {
       close.setBackground(new Color(238, 221, 130));
       close.setBorderPainted(false);
       close.setContentAreaFilled(false);
+      close.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
       setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
       add(text);
@@ -59,6 +71,7 @@ public class Helper extends JPanel implements KeyListener {
 
       editor.add(this);
       editor.addKeyListener(this);
+
       close.addMouseListener(new MouseAdapter() {
          @Override
          public void mouseClicked(MouseEvent e) {
@@ -70,14 +83,34 @@ public class Helper extends JPanel implements KeyListener {
 
    @Override
    public void keyPressed(KeyEvent ke) {
-      if (ke.getKeyCode() == KeyEvent.VK_F1) {
-         if (ke.isControlDown() || ke.isAltDown()) {
-            if (!isShowing()) {
-               refreshLocation();
-               setVisible(true);
-            } else {
-               setVisible(false);
-            }
+
+      // Ctrl + Space 자동완성 팝업
+      if (isStart && editor.getDocument().getLength() > 0) {
+         text.setText("Ctrl Space로 자동완성 팝업을 띄우실 수 있습니다.");
+         setVisible(true);
+         isStart = false;
+      }
+
+      // Ctrl + Backspace 한줄 삭제
+      else if (ke.getKeyChar() == KeyEvent.VK_BACK_SPACE) {
+         BackSpaceCount++;
+         if (BackSpaceCount > 5) {
+            text.setText("Ctrl BackSpace로 전부 지우소!!");
+            refreshLocation();
+            setVisible(true);
+            BackSpaceCount = 0;
+         }
+      }
+
+      // Ctrl + L 라인 이동
+      else if (ke.getKeyCode() == KeyEvent.VK_UP || ke.getKeyCode() == KeyEvent.VK_DOWN) {
+         lineCount++;
+         System.out.println("linecount " + lineCount);
+         if (lineCount > 3) {
+            text.setText("Ctrl L로 줄 이동을 하실수 있습니다");
+            refreshLocation();
+            setVisible(true);
+            lineCount = 0;
          }
       }
    }
@@ -101,5 +134,10 @@ public class Helper extends JPanel implements KeyListener {
       Point p = new Point(0, editor.getHeight() - this.getHeight());
       this.setLocation(p);
       repaint();
+   }
+
+   private int getCurLine() {
+      Element root = editor.getDocument().getDefaultRootElement();
+      return root.getElementIndex(editor.getCaretPosition()) + 1; // starts from 0
    }
 }
